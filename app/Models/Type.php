645e1289da1dbbb74f04ade9d4051a5152174c13
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2024-02-19 19:38:37 +0100

    Creazione del model Type

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function portfolios()
+    {
+        return $this->hasMany(Portfolio::class);
+    }
 }
